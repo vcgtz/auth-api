793fa32007f01c2e88de6020db17ceef10533cdc
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-08-12 08:05:11 -0500

    Format using prettier

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -4,18 +4,13 @@ const generateJWT = (id) =>
   new Promise((resolve, reject) => {
     const payload = { id };
 
-    jwt.sign(
-      payload,
-      process.env.SECRET,
-      { expiresIn: '1m' },
-      (err, token) => {
-        if (err) {
-          return reject(new Error('JWT error'));
-        }
-
-        return resolve(token);
+    jwt.sign(payload, process.env.SECRET, { expiresIn: '1m' }, (err, token) => {
+      if (err) {
+        return reject(new Error('JWT error'));
       }
-    );
+
+      return resolve(token);
+    });
   });
 
 module.exports = {
